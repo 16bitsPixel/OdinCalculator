260a21cc478e80b990ae781af764643a02a85ab6
16bitsPixel <brandonllanes16@gmail.com>
2023-08-08 01:42:48 -0700

    add basic math operator functions

---
+++ javascript.js
@@ -0,0 +1,21 @@
+// operation functions
+
+// function adds num1 and num2 together
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+// functions subtracts num2 from num1
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+// function mutiplies the numbers together
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+// function divides num2 from num1
+function divide(num1, num2) {
+    return num1 / num2;
+}
