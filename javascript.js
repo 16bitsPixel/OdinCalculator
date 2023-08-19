51df2b8d4e54269aa3fc64492a3c43fe1306a6d7
16bitsPixel <brandonllanes16@gmail.com>
2023-08-18 17:06:30 -0700

    one calculation functionality

--- javascript.js
+++ javascript.js
@@ -36,4 +36,95 @@ function operate(op, num1, num2) {
 }
 
 // ----------MAIN------------
-let num1, operator, num2;
+
+// variables for our calculations
+let num1 = "", operator = "", num2 = "", num3 = "";
+let result = 0;
+let operatorSet = false;
+
+// query selectors for changing texts
+let resultDisplay = document.querySelector(".resultDisplay");
+let exprDisplay = document.querySelector(".expression");
+
+// add event listeners to the buttons
+(Array.from(document.querySelectorAll("button"))).forEach(button => {
+    button.addEventListener("click", () => {
+        if (operatorSet == false) {
+            if (button.className == "number") {
+                num1 += button.id;
+            }
+            else if (button.className == "operator") {
+                switch (button.id) {
+                    case "clear":
+                        num1 = ""; operator = "";
+                        break;
+                    case "delete":
+                        num1 = num1.slice(0, -1);
+                        break;
+                    case "sign":
+                        if (num1.charAt(0) != "-") {
+                            num1 = "-" + num1;
+                        }
+                        else {
+                            num1 = num1.slice(1);
+                        }
+                        break;
+                    case "equals":
+                        resultDisplay = num1;
+                        break;
+                    default:
+                        operator = button.id;
+                        operatorSet = true;
+                }
+            }
+        }
+        else {
+            if (button.className == "number") {
+                num2 += button.id;
+            }
+            else if (button.className == "operator") {
+                switch (button.id) {
+                    case "clear":
+                        if (num2 == "") {
+                            operator = "";
+                            operatorSet = false;
+                        }
+                        else {
+                            num2 = "";
+                        }
+                        break;
+                    case "delete":
+                        if (num2 == "") {
+                            operator = "";
+                            operatorSet = false;
+                        }
+                        else {
+                            num2 = num2.slice(0, -1);
+                        }
+                        break;
+                    case "sign":
+                        if (num2.charAt(0) != "-") {
+                            num2 = "-" + num2;
+                        }
+                        else {
+                            num2 = num2.slice(1);
+                        }
+                        break;
+                    case "/":
+                        break;
+                    case "*":
+                        break;
+                    case "-":
+                        break;
+                    case "+":
+                        break;
+                    case "equals":
+                        result = operate(operator, Number(num1), Number(num2));
+                        break;
+                }
+            }
+        }
+        exprDisplay.textContent = num1 + " " + operator + " " + num2;
+        resultDisplay.textContent = result;
+    });
+});
